df07e25f43434ea8fb0d52bc230ae701704fb7f0
Mohammadxoja2005 <rustamxojayev.muhammadxoja@yandex.ru>
2023-06-27 00:08:42 +0500

    first commit

---
+++ src/layouts/navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+// styles
+// import styles from "./index.module.scss";
+
+const Navbar: FC = () => {
+    return (
+        <div>
+            navbar
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+// layouts
+import Navbar from '../../layouts/navbar/Navbar'
+
+const Home: FC = () => {
+    return (
+        <>
+            <Navbar />
+        </>
+    )
+}
+
+export default Home
